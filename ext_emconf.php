954073c7b8f8b04ff0152eb968acaba34d29f2d4
NITSAN Technologies <sanjay@nitsan.in>
2019-04-05 21:16:06 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,17 +14,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => '[NITSAN] All in One Slider Plugin',
 	'description' => 'One of the only TYPO3 extension which provides to use most popular jQuery slider plugins at your website. This TYPO3 extension provides to configure sliders eg., Nivo Slider, Royal Slider, Owlcarousel, Sliderjs Slider & more will be available in an upcoming version.',
 	'category' => 'plugin',
-	'author' => 'Sanjay Chauhan [NITSAN Technologies]',
+	'author' => 'NITSAN Technologies',
 	'author_email' => 'sanjay@nitsan.in',
 	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '2.0.0',
+	'version' => '2.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-8.9.9',
+			'typo3' => '6.0.0-8.9.99',
 		),
 		'conflicts' => array(
 		),
