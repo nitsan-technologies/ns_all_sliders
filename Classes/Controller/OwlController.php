d10aaf91a96e79168508c56323f23e2f14a2b459
Nitsan Technologies <sanjay@nitsan.in>
2020-01-27 18:39:27 +0530

    [TODO]Minor changes update

--- Classes/Controller/OwlController.php
+++ Classes/Controller/OwlController.php
@@ -91,55 +91,57 @@ class OwlController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 
             $GLOBALS['TSFE']->additionalFooterData[$this->extKey] .= "
                 <script>
-                    $('#owl-demo').owlCarousel({
-                        autoplay : " . (isset($this->settings['autoPlay']) && $this->settings['autoPlay'] != '' ? $this->settings['autoPlay'] : $constant['ConAutoPlay']) . ',                        
-                        nav : ' . (isset($this->settings['navigation']) && $this->settings['navigation'] != '' ? $this->settings['navigation'] : $constant['Connavigation']) . ',                        
-                        items : ' . (isset($this->settings['items']) && $this->settings['items'] != '' ? $this->settings['items'] : $constant['Conitems']) . ',
-                        lazyLoad : ' . (isset($this->settings['lazyLoad'])  && $this->settings['lazyLoad'] != '' ? $this->settings['lazyLoad'] : $constant['ConlazyLoad']) . ',
-                        mouseDrag:' . (isset($this->settings['mouseDrag']) && $this->settings['mouseDrag'] != '' ? $this->settings['mouseDrag'] : $constant['ConmouseDrag']) . ',
-                        touchDrag:' . (isset($this->settings['touchDrag']) && $this->settings['touchDrag'] != '' ? $this->settings['touchDrag'] : $constant['ContouchDrag']) . ',
-                        
-                        margin:' . (isset($this->settings['margin']) && $this->settings['margin'] != '' ? $this->settings['margin'] : $constant['Conmargin']) . ',
-                        loop:' . (isset($this->settings['loop']) && $this->settings['loop'] != '' ? $this->settings['loop'] : $constant['Conloop']) . ',
-                        
+                    (function($) {
+                        $('#owl-demo').owlCarousel({
+                            autoplay : " . (isset($this->settings['autoPlay']) && $this->settings['autoPlay'] != '' ? $this->settings['autoPlay'] : $constant['ConAutoPlay']) . ',                        
+                            nav : ' . (isset($this->settings['navigation']) && $this->settings['navigation'] != '' ? $this->settings['navigation'] : $constant['Connavigation']) . ',                        
+                            items : ' . (isset($this->settings['items']) && $this->settings['items'] != '' ? $this->settings['items'] : $constant['Conitems']) . ',
+                            lazyLoad : ' . (isset($this->settings['lazyLoad'])  && $this->settings['lazyLoad'] != '' ? $this->settings['lazyLoad'] : $constant['ConlazyLoad']) . ',
+                            mouseDrag:' . (isset($this->settings['mouseDrag']) && $this->settings['mouseDrag'] != '' ? $this->settings['mouseDrag'] : $constant['ConmouseDrag']) . ',
+                            touchDrag:' . (isset($this->settings['touchDrag']) && $this->settings['touchDrag'] != '' ? $this->settings['touchDrag'] : $constant['ContouchDrag']) . ',
+                            
+                            margin:' . (isset($this->settings['margin']) && $this->settings['margin'] != '' ? $this->settings['margin'] : $constant['Conmargin']) . ',
+                            loop:' . (isset($this->settings['loop']) && $this->settings['loop'] != '' ? $this->settings['loop'] : $constant['Conloop']) . ',
+                            
 
-                        pullDrag:' . (isset($this->settings['pullDrag']) && $this->settings['pullDrag'] != '' ? $this->settings['pullDrag'] : $constant['ConpullDrag']) . ',                      
-                        freeDrag:' . (isset($this->settings['freeDrag']) && $this->settings['freeDrag'] != '' ? $this->settings['freeDrag'] : $constant['ConfreeDrag']) . ',
-                        stagePadding:' . (isset($this->settings['stagePadding']) && $this->settings['stagePadding'] != '' ? $this->settings['stagePadding'] : $constant['ConstagePadding']) . ',
-                        merge:' . (isset($this->settings['merge']) && $this->settings['merge'] != '' ? $this->settings['merge'] : $constant['Conmerge']) . ',
-                        mergeFit:' . (isset($this->settings['mergeFit']) && $this->settings['mergeFit'] != '' ? $this->settings['mergeFit'] : $constant['ConmergeFit']) . ',
-                        autoWidth:' . (isset($this->settings['autoWidth']) && $this->settings['autoWidth'] != '' ? $this->settings['autoWidth'] : $constant['ConautoWidth']) . ',
-                        startPosition:' . (isset($this->settings['startPosition']) && $this->settings['startPosition'] != '' ? $this->settings['startPosition'] : $constant['ConstartPosition']) . ',
-                        URLhashListener:' . (isset($this->settings['URLhashListener']) && $this->settings['URLhashListener'] != '' ? $this->settings['URLhashListener'] : $constant['ConURLhashListener']) . ',
-                        rewind:' . (isset($this->settings['rewind']) && $this->settings['rewind'] != '' ? $this->settings['rewind'] : $constant['Conrewind']) . ",
-                        navElement:'" . (isset($this->settings['navElement']) && $this->settings['navElement'] != '' ? $this->settings['navElement'] : $constant['ConnavElement']) . "',
-                        slideBy:" . (isset($this->settings['slideBy']) && $this->settings['slideBy'] != '' ? $this->settings['slideBy'] : $constant['ConslideBy']) . ",
-                        slideTransition:'" . (isset($this->settings['slideTransition']) && $this->settings['slideTransition'] != '' ? $this->settings['slideTransition'] : $constant['ConslideTransition']) . "',
-                        dots:" . (isset($this->settings['dots']) && $this->settings['dots'] != '' ? $this->settings['dots'] : $constant['Condots']) . ',
-                        dotsEach:' . (isset($this->settings['dotsEach']) && $this->settings['dotsEach'] != '' ? $this->settings['dotsEach'] : $constant['CondotsEach']) . ',
-                        dotsData:' . (isset($this->settings['dotsData']) && $this->settings['dotsData'] != '' ? $this->settings['dotsData'] : $constant['CondotsData']) . ',
-                        lazyLoadEager:' . (isset($this->settings['lazyLoadEager']) && $this->settings['lazyLoadEager'] != '' ? $this->settings['lazyLoadEager'] : $constant['ConlazyLoadEager']) . ',
-                        autoplayTimeout:' . (isset($this->settings['autoplayTimeout']) && $this->settings['autoplayTimeout'] != '' ? $this->settings['autoplayTimeout'] : $constant['ConautoplayTimeout']) . ',
-                        autoplayHoverPause:' . (isset($this->settings['autoplayHoverPause']) && $this->settings['autoplayHoverPause'] != '' ? $this->settings['autoplayHoverPause'] : $constant['ConautoplayHoverPause']) . ',
-                        autoplaySpeed:' . (isset($this->settings['autoplaySpeed']) && $this->settings['autoplaySpeed'] != '' ? $this->settings['autoplaySpeed'] : $constant['ConautoplaySpeed']) . ',
-                        navSpeed:' . (isset($this->settings['navSpeed']) && $this->settings['navSpeed'] != '' ? $this->settings['navSpeed'] : $constant['ConnavSpeed']) . ',
-                        dotsSpeed:' . (isset($this->settings['dotsSpeed']) && $this->settings['dotsSpeed'] != '' ? $this->settings['dotsSpeed'] : $constant['CondotsSpeed']) . ',
-                        dragEndSpeed:' . (isset($this->settings['dragEndSpeed']) && $this->settings['dragEndSpeed'] != '' ? $this->settings['dragEndSpeed'] : $constant['CondragEndSpeed']) . ",
-                        smartSpeed: 450,
-                        animateOut:'" . (isset($this->settings['animateOut']) && $this->settings['animateOut'] != '' ? $this->settings['animateOut'] : $constant['ConanimateOut']) . "',
-                        animateIn:'" . (isset($this->settings['animateIn']) && $this->settings['animateIn'] != '' ? $this->settings['animateIn'] : $constant['ConanimateIn']) . "',
-                        fallbackEasing:'" . (isset($this->settings['fallbackEasing']) && $this->settings['fallbackEasing'] != '' ? $this->settings['fallbackEasing'] : $constant['ConfallbackEasing']) . "',
-                        info:" . (isset($this->settings['info']) && $this->settings['info'] != '' ? $this->settings['info'] : $constant['Coninfo']) . ',
-                        nestedItemSelector:' . (isset($this->settings['nestedItemSelector']) && $this->settings['nestedItemSelector'] != '' ? $this->settings['nestedItemSelector'] : $constant['ConnestedItemSelector']) . ",
-                        
-                        itemElement:'" . (isset($this->settings['itemElement']) && $this->settings['itemElement'] != '' ? $this->settings['itemElement'] : $constant['ConitemElement']) . "',                        
-                        navContainer:" . (isset($this->settings['navContainer']) && $this->settings['navContainer'] != '' ? $this->settings['navContainer'] : $constant['ConnavContainer']) . ',
-                        center:' . (isset($this->settings['center']) && $this->settings['center'] != '' ? $this->settings['center'] : $constant['Concenter']) . ',
+                            pullDrag:' . (isset($this->settings['pullDrag']) && $this->settings['pullDrag'] != '' ? $this->settings['pullDrag'] : $constant['ConpullDrag']) . ',                      
+                            freeDrag:' . (isset($this->settings['freeDrag']) && $this->settings['freeDrag'] != '' ? $this->settings['freeDrag'] : $constant['ConfreeDrag']) . ',
+                            stagePadding:' . (isset($this->settings['stagePadding']) && $this->settings['stagePadding'] != '' ? $this->settings['stagePadding'] : $constant['ConstagePadding']) . ',
+                            merge:' . (isset($this->settings['merge']) && $this->settings['merge'] != '' ? $this->settings['merge'] : $constant['Conmerge']) . ',
+                            mergeFit:' . (isset($this->settings['mergeFit']) && $this->settings['mergeFit'] != '' ? $this->settings['mergeFit'] : $constant['ConmergeFit']) . ',
+                            autoWidth:' . (isset($this->settings['autoWidth']) && $this->settings['autoWidth'] != '' ? $this->settings['autoWidth'] : $constant['ConautoWidth']) . ',
+                            startPosition:' . (isset($this->settings['startPosition']) && $this->settings['startPosition'] != '' ? $this->settings['startPosition'] : $constant['ConstartPosition']) . ',
+                            URLhashListener:' . (isset($this->settings['URLhashListener']) && $this->settings['URLhashListener'] != '' ? $this->settings['URLhashListener'] : $constant['ConURLhashListener']) . ',
+                            rewind:' . (isset($this->settings['rewind']) && $this->settings['rewind'] != '' ? $this->settings['rewind'] : $constant['Conrewind']) . ",
+                            navElement:'" . (isset($this->settings['navElement']) && $this->settings['navElement'] != '' ? $this->settings['navElement'] : $constant['ConnavElement']) . "',
+                            slideBy:" . (isset($this->settings['slideBy']) && $this->settings['slideBy'] != '' ? $this->settings['slideBy'] : $constant['ConslideBy']) . ",
+                            slideTransition:'" . (isset($this->settings['slideTransition']) && $this->settings['slideTransition'] != '' ? $this->settings['slideTransition'] : $constant['ConslideTransition']) . "',
+                            dots:" . (isset($this->settings['dots']) && $this->settings['dots'] != '' ? $this->settings['dots'] : $constant['Condots']) . ',
+                            dotsEach:' . (isset($this->settings['dotsEach']) && $this->settings['dotsEach'] != '' ? $this->settings['dotsEach'] : $constant['CondotsEach']) . ',
+                            dotsData:' . (isset($this->settings['dotsData']) && $this->settings['dotsData'] != '' ? $this->settings['dotsData'] : $constant['CondotsData']) . ',
+                            lazyLoadEager:' . (isset($this->settings['lazyLoadEager']) && $this->settings['lazyLoadEager'] != '' ? $this->settings['lazyLoadEager'] : $constant['ConlazyLoadEager']) . ',
+                            autoplayTimeout:' . (isset($this->settings['autoplayTimeout']) && $this->settings['autoplayTimeout'] != '' ? $this->settings['autoplayTimeout'] : $constant['ConautoplayTimeout']) . ',
+                            autoplayHoverPause:' . (isset($this->settings['autoplayHoverPause']) && $this->settings['autoplayHoverPause'] != '' ? $this->settings['autoplayHoverPause'] : $constant['ConautoplayHoverPause']) . ',
+                            autoplaySpeed:' . (isset($this->settings['autoplaySpeed']) && $this->settings['autoplaySpeed'] != '' ? $this->settings['autoplaySpeed'] : $constant['ConautoplaySpeed']) . ',
+                            navSpeed:' . (isset($this->settings['navSpeed']) && $this->settings['navSpeed'] != '' ? $this->settings['navSpeed'] : $constant['ConnavSpeed']) . ',
+                            dotsSpeed:' . (isset($this->settings['dotsSpeed']) && $this->settings['dotsSpeed'] != '' ? $this->settings['dotsSpeed'] : $constant['CondotsSpeed']) . ',
+                            dragEndSpeed:' . (isset($this->settings['dragEndSpeed']) && $this->settings['dragEndSpeed'] != '' ? $this->settings['dragEndSpeed'] : $constant['CondragEndSpeed']) . ",
+                            smartSpeed: 450,
+                            animateOut:'" . (isset($this->settings['animateOut']) && $this->settings['animateOut'] != '' ? $this->settings['animateOut'] : $constant['ConanimateOut']) . "',
+                            animateIn:'" . (isset($this->settings['animateIn']) && $this->settings['animateIn'] != '' ? $this->settings['animateIn'] : $constant['ConanimateIn']) . "',
+                            fallbackEasing:'" . (isset($this->settings['fallbackEasing']) && $this->settings['fallbackEasing'] != '' ? $this->settings['fallbackEasing'] : $constant['ConfallbackEasing']) . "',
+                            info:" . (isset($this->settings['info']) && $this->settings['info'] != '' ? $this->settings['info'] : $constant['Coninfo']) . ',
+                            nestedItemSelector:' . (isset($this->settings['nestedItemSelector']) && $this->settings['nestedItemSelector'] != '' ? $this->settings['nestedItemSelector'] : $constant['ConnestedItemSelector']) . ",
+                            
+                            itemElement:'" . (isset($this->settings['itemElement']) && $this->settings['itemElement'] != '' ? $this->settings['itemElement'] : $constant['ConitemElement']) . "',                        
+                            navContainer:" . (isset($this->settings['navContainer']) && $this->settings['navContainer'] != '' ? $this->settings['navContainer'] : $constant['ConnavContainer']) . ',
+                            center:' . (isset($this->settings['center']) && $this->settings['center'] != '' ? $this->settings['center'] : $constant['Concenter']) . ',
 
-                        dotsContainer:' . (isset($this->settings['dotsContainer']) && $this->settings['dotsContainer'] != '' ? $this->settings['dotsContainer'] : $constant['CondotsContainer']) . ',
-                        checkVisible:' . (isset($this->settings['checkVisible']) && $this->settings['checkVisible'] != '' ? $this->settings['checkVisible'] : $constant['ConcheckVisible']) . ',                        
-                        ' . $thumbs . "
-                    });
+                            dotsContainer:' . (isset($this->settings['dotsContainer']) && $this->settings['dotsContainer'] != '' ? $this->settings['dotsContainer'] : $constant['CondotsContainer']) . ',
+                            checkVisible:' . (isset($this->settings['checkVisible']) && $this->settings['checkVisible'] != '' ? $this->settings['checkVisible'] : $constant['ConcheckVisible']) . ',                        
+                            ' . $thumbs . "
+                        });
+                     })(jQuery);
                     function makePages() {
                         $.each(this.owl.userItems, function(i) {
                             $('.owl-controls .owl-page').eq(i)
