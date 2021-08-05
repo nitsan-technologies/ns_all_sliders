f9f3adfbaf36db0a80af7348425af94c54a19f33
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-08-05 18:13:52 +0530

    [TODO] Code improvements

--- ext_tables.php
+++ ext_tables.php
@@ -8,5 +8,5 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_nsallsliders_domain_model_gallery');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_all_sliders/Configuration/TSconfig/ContentElementWizard.txt">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_all_sliders/Configuration/TSconfig/ContentElementWizard.tsconfig">'
 );
